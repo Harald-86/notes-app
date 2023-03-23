ca85124368f74cf5de9d351dbd96bc9225add43d
Harald-86 <79138134+Harald-86@users.noreply.github.com>
2023-03-23 20:15:36 +0100

    Add note
    Legge til notat

--- app.js
+++ app.js
@@ -1,9 +1,63 @@
 const chalk = require("chalk");
-const getNotes = require("./notes.js");
+const notes = require("./notes.js");
+const yargs = require("yargs");
 
-const msg = getNotes();
-console.log(chalk.blue(msg));
+// Customize yargs version
 
-console.log(chalk.green.inverse.bold("Success!!"));
+yargs.version("1.1.0");
 
-console.log(chalk.blue.bold.inverse("I`m blue"));
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
+
+// Create remove command
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
+
+// Create list command
+
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: function () {
+    console.log("Listing out all notes");
+  },
+});
+
+// Create read commmand
+
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading a note");
+  },
+});
+
+yargs.parse();
+
+// Add, remove, read, list
+/* console.log(yargs.argv); */
